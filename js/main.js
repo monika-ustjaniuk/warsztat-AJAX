7f40bbfc579517d7b80250eeeb4bdf756d94deaa
monika-ustjaniuk <monikaustjaniuk@hotmail.com>
2022-01-11 14:50:00 +0100

    AJAX-warsztat

---
+++ js/main.js
@@ -0,0 +1,34 @@
+console.log('AJAX - Warsztat - Pobierz dane po kliknięciu');
+
+let btnGetData = document.getElementById('get-data');
+
+
+const getDat = () => {
+    fetch('https://akademia108.pl/api/ajax/get-post.php')
+        .then(res => res.json())
+        .then(data => {
+
+            let pId = document.createElement('p');
+            let pUserId = document.createElement('p');
+            let pTitle = document.createElement('p');
+            let pBody = document.createElement('p');
+            let hr = document.createElement('hr');
+
+            pId.innerText = `Post ID: ${data.id}`;
+            pUserId.innerText = `User ID: ${data.pUserId}`;
+            pTitle.innerText = `Title: ${data.title}`;
+            pBody.innerText = `Body: ${data.body}`;
+            
+            document.body.appendChild(pId);
+            document.body.appendChild(pUserId);
+            document.body.appendChild(pTitle);
+            document.body.appendChild(pBody);
+            document.body.appendChild(hr);
+        })
+        .catch(error => {
+            console.error(error);
+
+        })
+    
+}
+btnGetData.addEventListener('click', getDat);
